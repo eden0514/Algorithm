1115e845777c9e53416e9e425800d1c549fee62a
eden0514 <80727924+eden0514@users.noreply.github.com>
2021-12-10 17:26:06 +0900

    Update 폰켓몬.js

--- programmers_Level 1/폰켓몬.js
+++ programmers_Level 1/폰켓몬.js
@@ -1,3 +1,12 @@
+//Set이라는 메소드는 자료형 관계없이 원시타입이든, 참조형이든 유일한 값을 저장할 수 있는 메소드. 즉, 중복값을 없애줌
+//new Set([배열]) => {1,3,4} 이런 식으로 리턴이 됨. 이를 배열 형태로 변환을 해 길이를 구해줌
+function solution(nums) {
+    let half = Math.floor(nums.length/2);
+    let unique = [...new Set(nums)];
+    return half < unique.length ?  half: unique.length
+}
+
+/*
 function solution(nums) {
     //우선 nums의 갯수의 반보다 이상으로 가져갈 수 없으므로 길이의 반 값을 정수로
     let half = Math.floor(nums.length/2);
@@ -12,3 +21,5 @@ function solution(nums) {
     if(half < result.length) return half;
     else return result.length;
 }
+
+*/
