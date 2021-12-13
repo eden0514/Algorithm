7bcb7742aed0d0355e1312eb9014f244a47335aa
eden0514 <80727924+eden0514@users.noreply.github.com>
2021-12-13 13:58:02 +0900

    Update 자연수 뒤집어 배열로 만들기.js

--- programmers_Level 1/자연수 뒤집어 배열로 만들기.js
+++ programmers_Level 1/자연수 뒤집어 배열로 만들기.js
@@ -1,4 +1,11 @@
+//reverse()사용한 경우
 function solution(n) {
+    let arr = String(n).split('').reverse();
+    return arr.map(el=>Number(el));
+}
+
+
+/*function solution(n) {
   //인자인 자연수를 문자열로 변환하여 해당 값을 split을 사용해 배열로 변환
     let arr = String(n).split('');
   //배열을 뒤에서 앞으로 반복문을 돌리면서 result라는 배열에 해당 값을 숫자로 변환하여 추가하기
@@ -7,4 +14,4 @@ function solution(n) {
         result.push(Number(arr[i]))
     }
     return result;
-}
+}*/
