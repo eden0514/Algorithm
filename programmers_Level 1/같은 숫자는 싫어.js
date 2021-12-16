1cb2a4c1bf9fa1507c13df4ff5d5766554ee6b52
eden0514 <80727924+eden0514@users.noreply.github.com>
2021-12-16 22:28:31 +0900

    Update 같은 숫자는 싫어.js

--- programmers_Level 1/같은 숫자는 싫어.js
+++ programmers_Level 1/같은 숫자는 싫어.js
@@ -1,9 +1,12 @@
 //테스트는 통과하나, 효율성 테스트는 실패가 뜸
 // 배열 arr의 크기 : 1,000,000 이하의 자연수인 것으로 보아 O(n) 또는 O(log n)이어야 함.
-// 밑의 시간복잡도는 O(n)이지만 효율성에서 실패가 뜸. 그러면 O(n)보다 빠른 시간복잡도로 작성을 해야한다는 것 같음.
-// 효율성을 올리기 위해서는 for문말고 다른 걸 사용해야할 것 같음
-// 어떻게 해야할까...?
-
+// 효율성 검사가 통과되지 않는 코드는 for문을 2번 돌아서 그런가...?
+//효율성 검사 통과되는 코드 O(n)
+function solution(arr){
+    
+    return arr.filter((el,idx)=> el !== arr[idx+1])
+}
+/*
 function solution(arr){
     let result = [];
     for(let i = 0; i < arr.length -1; i++){
@@ -12,4 +15,4 @@ function solution(arr){
         }
     }
     return arr.filter((el,idx)=> !result.includes(idx))
-}
+}*/
