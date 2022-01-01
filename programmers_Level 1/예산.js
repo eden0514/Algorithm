66de4252a6737eff304c49f4533628d21cbe68bc
eden0514 <80727924+eden0514@users.noreply.github.com>
2022-01-01 23:20:12 +0900

    Create 예산.js

---
+++ programmers_Level 1/예산.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+  //인자 d를 오름차순으로 정렬을 해준다.
+    d.sort((a,b)=>a-b);
+    let count = 0;
+  //d를 돌면서 버짓의 값이 0이 되지 않는 경우까지 반복을 해준다. 해당 값을 뺀 경우에는 count를 올려준다.
+    for(let i = 0; i < d.length; i++){
+        if(budget - d[i] >= 0){
+           budget -= d[i];
+            count++; 
+        }
+    }
+    return count;
+}
