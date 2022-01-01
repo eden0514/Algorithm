169953ba249afa814dbcf6688885a1718893628b
eden0514 <80727924+eden0514@users.noreply.github.com>
2022-01-01 23:08:39 +0900

    Update 행렬의 덧셈.js

--- programmers_Level 1/행렬의 덧셈.js
+++ programmers_Level 1/행렬의 덧셈.js
@@ -1,3 +1,12 @@
+//for문으로 작성했던 부분을 map을 이용해 다시 작성해본 코드
+function solution(arr1, arr2) {
+    let result = arr1.map((el,index)=>{
+        return el.map((el,idx)=>el = el+arr2[index][idx])
+    })
+    return result;
+    
+}
+/*
 function solution(arr1, arr2) {
   //인자인 arr1의 값에 arr2의 같은 자리의 값을 더해준다.
     let result = arr1.map((el,idx)=>{
@@ -9,3 +18,4 @@ function solution(arr1, arr2) {
     return result;
     
 }
+*/
