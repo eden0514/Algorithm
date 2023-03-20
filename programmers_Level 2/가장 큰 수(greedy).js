1b4f80a875377fac1c231cf495252714168cc8a9
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-03-20 23:06:34 +0900

    Create 가장 큰 수(greedy).js
    탐욕 알고리즘 사용하는 방법 생각

---
+++ programmers_Level 2/가장 큰 수(greedy).js
@@ -0,0 +1,7 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42883?language=javascript
+function solution(number, k) {
+//해당 문제의 경우 탐욕 알고리즘으로 문제를 풀어야 하므로
+// 우선 받은 넘버를 각 숫자를 배열의 값으로 만들고
+  //해당 배열에서 제일 큰 값을 빼내어 임의의 배열에 추가한다.
+  //위의 부분을 number.length - k 번을 반복하여 제일 큰 수를 만들어낸다.
+}
