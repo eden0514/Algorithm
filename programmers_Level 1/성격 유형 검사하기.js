7eb6847ef1004eb02943a043bc02e8316edf675b
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-03-24 22:34:32 +0900

    Update 성격 유형 검사하기.js

--- programmers_Level 1/성격 유형 검사하기.js
+++ programmers_Level 1/성격 유형 검사하기.js
@@ -2,10 +2,10 @@ function solution(survey, choices) {
     let res = {"R":0,"T":0,"C":0,"F":0,"J":0,"M":0,"A":0,"N":0}
     let result="";
     for (let i = 0; i < survey.length ; i++){
-        if (choices[i] > 4){
-            res[survey[i][0]] += choices[i] - 4
-        }else if(choices[i] < 4){
-            res[survey[i][1]] += 4 - choices[i]
+        if (choices[i] < 4){
+            res[survey[i][0]] += 4 - choices[i]
+        }else if(choices[i] > 4){
+            res[survey[i][1]] += choices[i]- 4
         }
     }
     if (res["R"] >= res["T"]){
@@ -31,4 +31,5 @@ function solution(survey, choices) {
     return result;
 }
 
-//답이 틀리다. 그리구 더 효율적으로 작성할 수 있는 방법이 있는지 찾아보자. 배열을 사용하면 저런 if문 사용하지 않고 할 수 있을까??
+//어제 문제를 틀린 이유 : 문제를 꼼꼼히 보지 않아서 반대로 값을 주고 있었음
+// 저렇게 static하게 작성하는 거 말고 다른 방법 확인하기
