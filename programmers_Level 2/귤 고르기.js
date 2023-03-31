29b9c0b5b9fd0c6192b30d25f38a479e9a0d8ec4
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-03-31 21:34:42 +0900

    Update 귤 고르기.js

--- programmers_Level 2/귤 고르기.js
+++ programmers_Level 2/귤 고르기.js
@@ -1,1 +1,9 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/138476
+
+function solution(k, tangerine) {
+// tangerine을 오름차순으로 정렬
+// k 개수만큼 자르고
+//해당 배열에서 동일한 값이 몇 개인지 파악
+// 그 중 제일 작은 값을 리턴
+  //시간복잡도도 고려 해보기
+}
