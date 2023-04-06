dc50ff96d3229009901607ae1407c1cd67a1d9b4
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-04-07 08:53:25 +0900

    Update 괄호 회전하기.js
    bracket 개수를 객체 vlaue로 저장

--- programmers_Level 2/괄호 회전하기.js
+++ programmers_Level 2/괄호 회전하기.js
@@ -1,7 +1,6 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/76502
 function solution(s) {
-    let bracket = ['(', ')', '[', ']'], ['{', '}']]
-    let result = [[0,0],[0,0],[0,0]]
+    let bracket = {'(':0,')':0,'{':0,'}':0,'[':0,']':0}
     //배열의 2번째 값이 먼저 1이 되는 경우는 카운트 하지 않는다
     //배열의 앞자리가 바뀐 뒤 뒤의 자리 값이 1 추가 되면 result 값을 1 추가한 뒤 해당 배열의 값을 0으로 초기화한다.
     for(let i = 0; i < s.length; i++){
