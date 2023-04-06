6b43cad3f62d32e6451c1482e73e05748932f132
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-04-06 13:01:57 +0900

    Update 괄호 회전하기.js

--- programmers_Level 2/괄호 회전하기.js
+++ programmers_Level 2/괄호 회전하기.js
@@ -1,6 +1,10 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/76502
 function solution(s) {
-  //열리는 괄호가 먼저 오는지 체크
-  //열린 괄호만큼 닫힌 괄호가 있는지 체크
-  //위의 내용을 충족하면 1씩 값을 증가 시켜줌
+    let bracket = ['(', ')', '[', ']'], ['{', '}']]
+    let result = [[0,0],[0,0],[0,0]]
+    //배열의 2번째 값이 먼저 1이 되는 경우는 카운트 하지 않는다
+    //배열의 앞자리가 바뀐 뒤 뒤의 자리 값이 1 추가 되면 result 값을 1 추가한 뒤 해당 배열의 값을 0으로 초기화한다.
+    for(let i = 0; i < s.length; i++){
+        
+    }
 }
