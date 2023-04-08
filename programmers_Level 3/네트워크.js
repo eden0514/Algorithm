8e906f738d3fb2dee2db9b507e9c861434c302f6
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-04-08 16:30:51 +0900

    Create 네트워크.js
    DFS 문제

---
+++ programmers_Level 3/네트워크.js
@@ -0,0 +1,23 @@
+function solution(n, computers) {
+    let result = 0;
+    let visited = Array(n).fill(false);
+    
+    function dfs(idx){
+        visited[idx] = true;
+        
+        for(let i=0; i < n; i++){
+            if (computers[idx][i] && !visited[i]){
+                dfs(i)
+            }
+        }
+
+    }
+    
+    for (let c = 0 ; c <n ; c++){
+        if (!visited[c]){
+            dfs(c);
+            result++
+        }
+    }
+    return result;
+}
