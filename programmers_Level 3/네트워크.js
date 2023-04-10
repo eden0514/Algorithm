5fc28b11e3b154c436e0ffb462b1f59a96f50149
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-04-10 23:18:39 +0900

    Update 네트워크.js

--- programmers_Level 3/네트워크.js
+++ programmers_Level 3/네트워크.js
@@ -22,3 +22,7 @@ function solution(n, computers) {
     return result;
 }
 //bfs로도 풀어보기!
+function solution(n, computers){
+//전체적으로 dfs 랑 비슷하지만 dfs의 경우 방문하지 않고 나와 연결된 컴퓨터의 경우 재귀적으로 함수를 호출하여 완전탐색하지만 
+//bfs의 경우 아직 방문하지 않은 나와 연결된 컴퓨터를 queue에 넣어서 queue 순서로 컴퓨터를 점검
+}
