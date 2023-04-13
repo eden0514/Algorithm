7894b3e9a7c2cf3959022382e21d8dbcf03d4335
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-04-13 23:15:02 +0900

    Update 달리기 경주.js

--- programmers_Level 1/달리기 경주.js
+++ programmers_Level 1/달리기 경주.js
@@ -17,3 +17,4 @@ function solution(players, callings) {
 //해당 코드의 경우 시간초과로 최적화 필요
 //indexOf 사용 부분 변경 필요 - players 배열을 선형 검색하여 값을 찾기 때문
 //player를 key로 index 값을 value로
+//이렇게 바꾸면 실패 -> pObj가 고정값으로 존재하므로 이미 calling이 된 친구는 변한 값으로 존재해야함.... 다시 생각해보기
