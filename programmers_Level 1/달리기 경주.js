58ceff94c7984fd2d0b6d3c1531338548454b908
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-04-13 23:10:37 +0900

    Update 달리기 경주.js

--- programmers_Level 1/달리기 경주.js
+++ programmers_Level 1/달리기 경주.js
@@ -1,6 +1,11 @@
 function solution(players, callings) {
+    let pObj = {};
+    for(let i =0; i < players.length; i++){
+        pObj[players[i]] = i
+    }
+    
     for (let i = 0; i < callings.length; i++){
-        let cIdx = players.indexOf(callings[i])
+        let cIdx = pObj[callings[i]]
         let bIdx = cIdx-1
         if (bIdx < 0) {
             continue;
@@ -11,3 +16,4 @@ function solution(players, callings) {
 }
 //해당 코드의 경우 시간초과로 최적화 필요
 //indexOf 사용 부분 변경 필요 - players 배열을 선형 검색하여 값을 찾기 때문
+//player를 key로 index 값을 value로
