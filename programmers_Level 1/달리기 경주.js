188de3d56628663275dd1f4a35ebfe79d9a0218f
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-04-14 21:47:34 +0900

    Update 달리기 경주.js

--- programmers_Level 1/달리기 경주.js
+++ programmers_Level 1/달리기 경주.js
@@ -18,3 +18,4 @@ function solution(players, callings) {
 //indexOf 사용 부분 변경 필요 - players 배열을 선형 검색하여 값을 찾기 때문
 //player를 key로 index 값을 value로
 //이렇게 바꾸면 실패 -> pObj가 고정값으로 존재하므로 이미 calling이 된 친구는 변한 값으로 존재해야함.... 다시 생각해보기
+//배열로 하지 않고 객체 value 값을 변경하여 최종적으로 value 순서대로 다시 배열로 만들면??
