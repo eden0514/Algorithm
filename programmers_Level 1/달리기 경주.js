9f892ff89a21ce674836f75ebdcad0f541905dfa
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-04-17 21:13:37 +0900

    Update 달리기 경주.js

--- programmers_Level 1/달리기 경주.js
+++ programmers_Level 1/달리기 경주.js
@@ -18,4 +18,7 @@ function solution(players, callings) {
 //indexOf 사용 부분 변경 필요 - players 배열을 선형 검색하여 값을 찾기 때문
 //player를 key로 index 값을 value로
 //이렇게 바꾸면 실패 -> pObj가 고정값으로 존재하므로 이미 calling이 된 친구는 변한 값으로 존재해야함.... 다시 생각해보기
-//순회하지 않고 어떻게 값을 찾을 수 있을까
+//객체 {index : 이름 }
+//calling 반복문 
+//value 값으로 key값 찾을 수 있다면 
+// 두 값을 찾아서 
