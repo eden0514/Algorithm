e16bf68d31958304d114aaf8083f7ec5fcc799f2
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-05-07 21:24:04 +0900

    Create 카드 뭉치.js

---
+++ programmers_Level 1/카드 뭉치.js
@@ -0,0 +1,8 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/159994
+
+function solution(cards1, cards2, goal) {
+    //cards1과 cards2의 순서대로 goal을 만들 수 있느냐?
+    //두 배열의 앞의 값을 빼서 사용 가능한지 확인
+    // 두 배열 값 중 하나라도 쓸 수 없으면 no
+    
+}
