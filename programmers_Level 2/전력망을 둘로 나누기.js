84e7ccc4ce0a59ad90362a9055ace058a88bf98b
SUKYUNG KIM <80727924+eden0514@users.noreply.github.com>
2023-05-17 13:39:24 +0900

    Update 전력망을 둘로 나누기.js

--- programmers_Level 2/전력망을 둘로 나누기.js
+++ programmers_Level 2/전력망을 둘로 나누기.js
@@ -1,1 +1,4 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/86971
+//https://school.programmers.co.kr/learn/courses/30/lessons/86971
+//wires의 값으로 들어온 선 들을 돌아가면서 삭제
+//두 전력망으로 나눠졌을 때 nodes 개수 차이 확인
+//제일 작은 값으로 차이 나는 값을 return
